b6cf903bad00b8e20434ffa42f507b6fc11e6317
Manikanta-2000 <manikantaganti@gmail.com>
2024-12-26 11:15:22 -0500

    small changes

--- handlers/users/logout.js
+++ handlers/users/logout.js
@@ -2,9 +2,8 @@
 function logout(req,res){
     res.cookie("jwt","",{
         httpOnly: true,
-        signed:true
     })
     return res.status(200).send("Successfully Logged out")
 }
 
-module.exports=logout
+module.exports=logout 
